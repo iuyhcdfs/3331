9ae0b5a6be5bf9d8b3e628ad96fa642e4f167be0
iuyhcdfs <iuyhcdfs@gmail.com>
2014-10-28 13:49:41 +1100

    using structs to actually pass functions oh god hayfever

--- RoutingPerformance.c
+++ RoutingPerformance.c
@@ -1,8 +1,8 @@
 /*
-	make virtual network
-	test simple routing algorithms on virtual network
-	dont forget the report!
-   */
+//	make virtual network
+//	test simple routing algorithms on virtual network
+//	dont forget the report!
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,7 +12,35 @@
 int routeSHP();
 int routeSDP();
 int routeLLP();
-    
+
+// class: word for convenience
+typedef struct {
+    char * text;
+    int length;
+} _word;
+typedef *_word Word;
+
+// class: request for network load
+typedef struct {
+
+} _request;
+typedef *_request Request;
+
+// input a string get the index of the array it is meant to be
+int getStringIndex(Word wordList[], int listLength, char* string){
+    // start w/ failure condition
+    int result = -1
+    // scroll until we find a match
+    // assumed the list has only unique entries
+    for(int x = 0; x < listLength; x++){
+        if(strcmp(wordList[x]->text, string)){
+            result = x;
+        }
+    } 
+    return result;
+}
+
+
 int main (int argc, char* argv[]) {
     // Process args and store their values - 5 args
     char * network_scheme = argv[1];
@@ -20,7 +48,7 @@ int main (int argc, char* argv[]) {
     char * topology_file = argv[3];
     char * workload_file = argv[4];
     int packet_rate = atoi(argv[5]);
-    
+
     // ./RoutingPerformance CIRCUIT SHP topology.txt workload.txt 2 
     printf("DEBUG args read: %s %s %s %s %s %d\n", argv[0], network_scheme, routing_scheme, topology_file, workload_file, packet_rate);
 
@@ -30,27 +58,44 @@ int main (int argc, char* argv[]) {
     FILE * wFile;
     wFile = fopen(workload_file, "rt");
 
-    // find size of items in file
+    char buffer[100];
+    // find number of lines in file
+    char * names[50];
 
+    int nameCount = 0;
+    while(EOF != fscanf(tFile, "%[^\n]\n", buffer)){
+        printf("%s\n",buffer);
+        for(){
 
-    // allocate ajacency matrix for number of items. int stores their distance.
-    char ** names;
-    int ** networkMap;
+        }
+    }
 
+    // create adjacency matrix for connections, -1 means not connected
+    int networkMap[nameCount][nameCount];
+    for(int x = 0; x < nameCount; x++){
+        for(int y = 0; y < nameCount; y++){
+            networkMap[x][y] = -1;
+        }
+    }
+    printf("%d\n",networkMap[4][4]);
+    getStringIndex(names, 4, buffer);
 
+    printf("wat\n");
     // run algorithms, print out specific results in standard output
-    /*
-        total number of virtual circuit requests: 200
-        total number of packets: 4589
-        number of successfully routed packets: 3654
-        percentage of successfully routed packets: 79.63
-        number of blocked packets: 935
-        percentage of blocked packets: 20.37
-        average number of hops per circuit: 5.42
-        average cumulative propagation delay per circuit: 120.54
-    */    
-
-
+/*
+    total number of virtual circuit requests: 200
+    total number of packets: 4589
+    number of successfully routed packets: 3654
+    percentage of successfully routed packets: 79.63
+    number of blocked packets: 935
+    percentage of blocked packets: 20.37
+    average number of hops per circuit: 5.42
+    average cumulative propagation delay per circuit: 120.54
+*/    
     
     return EXIT_SUCCESS;
+
 }
+
+
+
